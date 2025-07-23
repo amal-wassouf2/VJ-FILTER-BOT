5c113620ddb614fecf323b600c1a9b29664f7e04
kin615 <kan604680@gmail.com>
2025-07-23 13:54:36 +0300

    info.py

--- info.py
+++ info.py
@@ -85,7 +85,7 @@ PUBLIC_FILE_CHANNEL = environ.get('PUBLIC_FILE_CHANNEL', '') # Public Channel Us
 # Links
 GRP_LNK = environ.get('GRP_LNK', 'https://t.me/Cima_Paradiso_Reg_Bot')
 CHNL_LNK = environ.get('CHNL_LNK', 'https://t.me/cimaparadiso')
-SUPPORT_CHAT = environ.get('SUPPORT_CHAT', 'https://t.me/kinanalyousef') # Support Chat Link Without https:// or @
+SUPPORT_CHAT = environ.get('SUPPORT_CHAT', 'https://t.me/cimaparadisomovieserie') # Support Chat Link Without https:// or @
 OWNER_LNK = environ.get('OWNER_LNK', 'https://t.me/kinanalyousef')
 
 # True Or False
